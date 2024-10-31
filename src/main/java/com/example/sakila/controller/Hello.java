8713501906a94dc1daf9866f86d5c15a711fd3d2
GOODEE <GOODEE@DESKTOP-266LLB2>
2024-10-31 12:15:20 +0900

    application.properties) Log Setting

---
+++ src/main/java/com/example/sakila/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.example.sakila.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class Hello {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		log.debug("GG");
+		return "hello";
+	}
+
+}
